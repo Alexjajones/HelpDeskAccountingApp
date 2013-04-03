aa4e44664f06d306fa133c3f24f44ad19169357f
Alex Jones <alexjajones1@gmail.com>
2013-04-03 12:54:44 +0100

    Adding the class structures
    Based on the diagrams we made yesterday

---
+++ HelpDeskPayroll/HelpDeskPayroll/Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class WorkForcePay{
+	int Total, Senior, Ordinary, Contractor, DefaultSalaried, DefaultHourly, DefaultPayIncrease;
+public:
+	void CalculateTotalPays();
+};
+
+class Employee{
+	int ID, EmployeeType;
+	double Name, Position;
+public:
+	void GetName();
+};
+
+class SalriedStaff : public Employee{ 
+public:
+	void CalculatePay();
+	void GetPay();
+};
+
+class SeniorStaff : public SalriedStaff{ 
+int Salary, PayIncrease;
+};
+
+class OrdinaryStaff : public SalriedStaff{ 
+int Salary, PayIncrease;
+};
+
+
+class HourlyStaff : public Employee{ 
+public:
+	void CalculatePay();
+	void GetPay();
+};
+
+class Contractor : public HourlyStaff{
+int Salary;
+};
+
+struct EmployeeTypes{
+	int Senior, Ordinary, Contractor;
+};
+
+int main ()
+{
+	return 0;
+}
